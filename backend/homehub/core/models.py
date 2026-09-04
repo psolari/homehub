b1a62895b5583bfb5046b5ee2e49a737fe5187e8
Paolo Solari <paolosolari1998@gmail.com>
2026-09-05 00:45:10 +0100

    feat: export dashboard groups

--- backend/homehub/core/models.py
+++ backend/homehub/core/models.py
@@ -1,5 +1,5 @@
 from homehub.core.accounts.models import User
-from homehub.core.devices.models import DashboardCard, Device, DeviceLocation, IntegrationAccount
+from homehub.core.devices.models import DashboardCard, DashboardGroup, Device, DeviceLocation, IntegrationAccount
 from homehub.core.spaces.models import FloorPlan, FloorPlanObject, Room
 
-__all__ = ["DashboardCard","Device","DeviceLocation","FloorPlan","FloorPlanObject","IntegrationAccount","Room","User"]
+__all__ = ["DashboardCard","DashboardGroup","Device","DeviceLocation","FloorPlan","FloorPlanObject","IntegrationAccount","Room","User"]
